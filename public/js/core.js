89960ffe16c072ea014a1d0b4110779f145ce42f
Robert Gabriel <robert_gabriel@outlook.com>
2016-03-06 14:23:06 +0000

    Updated styles and layouts

--- public/js/core.js
+++ public/js/core.js
@@ -77,4 +77,4 @@ app.controller('controller', function ($scope) {
 $("img").error(function () {
     $(this).unbind("error").attr("src", "https://s3.amazonaws.com/uifaces/faces/twitter/rogie/128.jpg");
 });
-$('.list-group.nothide').slice(19).hide();
+$('.nothide').slice(19).hide();
